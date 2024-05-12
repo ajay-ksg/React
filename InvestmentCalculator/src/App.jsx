286d0a92a54be48e5cb2fbdc2e638097489502c5
Ajay Kumar <ajay.kumar@thoughtworks.com>
2024-05-12 16:56:54 +0530

    Adds simple investment calculator

--- InvestmentCalculator/src/App.jsx
+++ InvestmentCalculator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useInsertionEffect, useState } from "react"
+import {useState } from "react"
 import Header from "./components/Header"
 import Result from "./components/Result";
 import UserInput from "./components/UserInput"
@@ -25,12 +25,35 @@ function App() {
       });
   }
   const isValidInput = userInput.duration && userInput.duration > 0 && userInput.duration <= 150;
+  const [userInput, setUserInput] = useState(
+    {
+      initialInvestment: 1000,
+      annualInvestment: 100,
+      expectedReturn: 6,
+      duration: 6
+
+    }
+  );
+
+  function handleValueChange(inputIdentifier, newValue){
+  
+    setUserInput((prevUserInput) => 
+      {
+        return {
+          ...prevUserInput,
+          [inputIdentifier]: +newValue,
+        };
+      });
+  }
+  const isValidInput = userInput.duration && userInput.duration > 0 && userInput.duration <= 150;
   return (
     <>
       <Header />
       <UserInput input={userInput} handleInputChange={handleValueChange}/>
-      {isValidInput && <Result input={userInput} />}
-      {!isValidInput && <p className="center">Please enter a valid duration between 0 to 150 years</p>}
+      {isValidInput ? 
+        <Result input={userInput} />:
+        <p className="center">Please enter a valid duration between 0 to 150 years</p>
+      }
     </>
   )
 }
--- InvestmentCalculator/src/components/Input.jsx
+++ InvestmentCalculator/src/components/Input.jsx
@@ -3,6 +3,7 @@ export default function Input({lableText, identifier, value, onInputChange}){
         <p>
            <label>{lableText}</label>
            <input type="number" 
+           min={identifier ==='expectedReturn'|| identifier ==='duration'? 0: undefined}
            required value={value[identifier]} 
            onChange={(event) => onInputChange(identifier, event.target.value)}/>
          </p>
