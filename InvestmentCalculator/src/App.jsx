39e898933ed4ebeeb1df7a3f45bad1327ad0db83
Ajay Kumar <ajay.kumar@thoughtworks.com>
2024-05-12 17:14:45 +0530

    fix issues in investment calculator app

--- InvestmentCalculator/src/App.jsx
+++ InvestmentCalculator/src/App.jsx
@@ -25,27 +25,6 @@ function App() {
       });
   }
   const isValidInput = userInput.duration && userInput.duration > 0 && userInput.duration <= 150;
-  const [userInput, setUserInput] = useState(
-    {
-      initialInvestment: 1000,
-      annualInvestment: 100,
-      expectedReturn: 6,
-      duration: 6
-
-    }
-  );
-
-  function handleValueChange(inputIdentifier, newValue){
-  
-    setUserInput((prevUserInput) => 
-      {
-        return {
-          ...prevUserInput,
-          [inputIdentifier]: +newValue,
-        };
-      });
-  }
-  const isValidInput = userInput.duration && userInput.duration > 0 && userInput.duration <= 150;
   return (
     <>
       <Header />
